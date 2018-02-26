1b0279f5c18ba3a5c2207f91ada1082a050f3f02
Joshua Emmons <skia@skia.net>
2018-02-26 10:59:06 -0600

    Adds conditional mutation to ValidJSONObject

--- Sources/ValidJSONObject.swift
+++ Sources/ValidJSONObject.swift
@@ -1,11 +1,48 @@
 import Foundation
 
 
+
 public struct ValidJSONObject {
-  public let value: JSONObject
+  public private(set) var value: JSONObject
+  
   
   public init(_ json: JSONObject) throws {
     try JSONHelper.validate(json)
     value = json
+  }
+  
+  
+  /**
+   Sets (i.e. *associates*) `value` with a given `key`. Blows up if `value` is not valid JSON.
+   
+   - parameter key: The key to associate `value` with — if `value` is valid JSON.
+   - parameter value: A JSON value to be associated with `key`. An error will be thrown if this is not valid JSON.
+   
+   - throws: `JSONError.invalid` if `value` isn't valid JSON.
+   
+   - note: We'd like to model this as a `subscript` instead, but there's currently no way to make a subscript setter `throw`.
+   */
+  mutating public func associate(key: String, value: Any) throws {
+    try JSONHelper.validate([value])
+    self.value[key] = value
+  }
+  
+  
+  /**
+   Sets (i.e. *associates*) `value` for a given `key`, but only if `value` is valid JSON.
+   
+   - parameter key: The key to associate `value` with — if `value` is valid JSON.
+   - parameter value: A JSON value to be associated with `key`. If it is not valid JSON, it will not be associated.
+   - returns: `true` if `value` is valid JSON and `key` and `value` are successfully associated. `false` if `value` is not valid JSON.
+   
+   - note: It'd make sense to model this as a `subscript`, but subscripts are generally thought of as assignment in Swift. A conditional subscript would be weird and unexpected behavior.
+   */
+  @discardableResult mutating public func associateIfValid(key: String, value: Any) -> Bool {
+    do {
+      try associate(key: key, value: value)
+      return true
+    } catch {
+      return false
+    }
   }
 }
--- Tests/ValidJSONObjectTests.swift
+++ Tests/ValidJSONObjectTests.swift
@@ -3,9 +3,20 @@ import Medea
 
 
 class ValidJSONObjectTests: XCTestCase {
+  let aKey = "foo"
+  let aString = "string"
+  let anInt = 42
+  let aFloat = 3.14
+  let aBool = true
+  lazy var anArray: JSONArray = [self.aString, self.anInt, self.aBool]
+  lazy var anObject: JSONObject = [self.aKey: self.aString]
+  lazy var aNestedArray: JSONArray = [self.aString, self.anInt, self.anObject]
+  lazy var aNestedObject: JSONObject = [self.aKey: self.anArray]
+  
+  
   func testValid() {
-    let subject = try! ValidJSONObject(["foo":"bar"])
-    XCTAssertEqual(subject.value["foo"] as! String, "bar")
+    let subject = try! ValidJSONObject(anObject)
+    XCTAssertEqual(aString, subject.value[aKey] as! String)
   }
   
   
@@ -21,5 +32,52 @@ class ValidJSONObjectTests: XCTestCase {
     }
     
     wait(for: [expectedThrow], timeout: 1)
+  }
+  
+  
+  func testValidMutability() {
+    var subject = try! ValidJSONObject([:])
+    try! subject.associate(key: "string", value: aString)
+    try! subject.associate(key: "int", value: anInt)
+    try! subject.associate(key: "float", value: aFloat)
+    try! subject.associate(key: "bool", value: aBool)
+    try! subject.associate(key: "array", value: anArray)
+    try! subject.associate(key: "object", value: anObject)
+    try! subject.associate(key: "nested array", value: aNestedArray)
+    try! subject.associate(key: "nested object", value: aNestedObject)
+    
+    XCTAssertEqual(aString, subject.value["string"] as! String)
+    XCTAssertEqual(anInt, subject.value["int"] as! Int)
+    XCTAssertEqual(aFloat, subject.value["float"] as! Double)
+    XCTAssertEqual(aBool, subject.value["bool"] as! Bool)
+    XCTAssertEqual(aString, (subject.value["array"] as! JSONArray).first as! String)
+    XCTAssertEqual(aString, (subject.value["object"] as! JSONObject)[aKey] as! String)
+    XCTAssertEqual(aString, ((subject.value["nested array"] as! JSONArray).last as! JSONObject)[aKey] as! String)
+    XCTAssertEqual(aString, ((subject.value["nested object"] as! JSONObject)[aKey] as! JSONArray).first as! String)
+  }
+  
+  
+  func testInvalidMutability() {
+    let expectedThrow = expectation(description: "Waiting for failure associating non-JSON value.")
+    var subject = try! ValidJSONObject([:])
+    do {
+      try subject.associate(key: aKey, value: Date())
+    } catch JSONError.invalidType {
+      expectedThrow.fulfill()
+    } catch {
+      XCTFail()
+    }
+    
+    wait(for: [expectedThrow], timeout: 1)
+  }
+  
+  
+  func testConditionalAssociation() {
+    var subject = try! ValidJSONObject([:])
+    XCTAssert(subject.associateIfValid(key: "foo", value: "bar"))
+    XCTAssertEqual("bar", subject.value["foo"] as! String)
+    
+    XCTAssertFalse(subject.associateIfValid(key: "bar", value: Date()))
+    XCTAssertNil(subject.value["bar"])
   }
 }
