affa0e81d4b0b8ebe9faf90272c683efe4a3a51b
Joshua Emmons <skia@skia.net>
2019-09-16 15:37:07 -0500

    Updates project for SwiftPM. Kills Carthage support.
    * Adds package file (`package.swift`)
    * Moves sources to default SwiftPM location.
    * Moves tests to default SwiftPM location.
    * Deletes project files and artifacts.
    * Swaps out Carthage badge for SwiftPM one.
    * Updates docs.
    * Deletes doc scripts (jazzy runs with default settings now).
    * Updates `.gitignore`.

--- Tests/MedeaTests/JSONFromFileTests.swift
+++ Tests/MedeaTests/JSONFromFileTests.swift
@@ -19,25 +19,26 @@ class JSONFromFileTests: XCTestCase {
   
   
   func testFileWithoutExtension() {
-    let json = try? JSONHelper.anyJSON(fromFileNamed: "object", bundle: Bundle(for: type(of: self)))
+    print("!!!\(fetchFakeBundle())")
+    let json = try? JSONHelper.anyJSON(fromFileNamed: "object", bundle: fetchFakeBundle())
     XCTAssertNotNil(json)
   }
   
   
   func testFileWithExtension() {
-    let json = try? JSONHelper.anyJSON(fromFileNamed: "object", extension: "ext", bundle: Bundle(for: type(of: self)))
+    let json = try? JSONHelper.anyJSON(fromFileNamed: "object", extension: "ext", bundle: fetchFakeBundle())
     XCTAssertNotNil(json)
   }
   
   
   func testObjectFile() {
-    let json = try! JSONHelper.jsonObject(fromFileNamed: "object", bundle: Bundle(for: type(of: self)))
+    let json = try! JSONHelper.jsonObject(fromFileNamed: "object", bundle: fetchFakeBundle())
     XCTAssertEqual(json["hello"] as! String, "world")
   }
   
   
   func testArrayFile() {
-    let json = try! JSONHelper.jsonArray(fromFileNamed: "array", bundle: Bundle(for: type(of: self)))
+    let json = try! JSONHelper.jsonArray(fromFileNamed: "array", bundle: fetchFakeBundle())
     XCTAssertEqual(json.count, 3)
     XCTAssertEqual(json[0] as! Int, 1)
     XCTAssertEqual(json[1] as! Int, 2)
